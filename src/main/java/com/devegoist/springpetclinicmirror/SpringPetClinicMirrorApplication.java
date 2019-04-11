a66767bee16d46d9c7a5bd6f0660352bb16ffe65
devegoist <devegoist@gmail.com>
2019-04-11 21:21:11 +0900

    init

---
+++ src/main/java/com/devegoist/springpetclinicmirror/SpringPetClinicMirrorApplication.java
@@ -0,0 +1,13 @@
+package com.devegoist.springpetclinicmirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicMirrorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicMirrorApplication.class, args);
+    }
+
+}
